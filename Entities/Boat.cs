2d780163904117731cfdfb18924c3c2f1da96393
ricardo.nunesdeo <ririxavier99@gmail.com>
2021-02-02 12:03:15 +0100

    Added testing files.

---
+++ Entities/Boat.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace boat_app.Entities
+{
+    public class Boat
+    {
+
+        private string name;
+        private string model;
+
+        public Boat() { }
+
+        public Boat(string bName, string bModel)
+        {
+            name = bName;
+            model = bModel;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Model;
+        }
+
+    }
+}

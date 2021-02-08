2c6433c9bdbe320983df0a0dac50c0e97dd62f05
ricardo.nunesdeo <ririxavier99@gmail.com>
2021-02-08 10:55:56 +0100

    Removed useless route.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,10 +10,12 @@ namespace boat_app.Controllers
     public class HomeController : Controller
     {
         // GET: HomeController
+        /*
         public ActionResult Index()
         {
             return View();
         }
+        */
 
         // GET: HomeController/Details/5
         public ActionResult Details(int id)

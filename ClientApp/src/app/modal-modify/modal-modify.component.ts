564dd233c9be3f546bc0126c0c70d489086a40a3
ricardo.nunesdeo <ririxavier99@gmail.com>
2021-02-09 09:10:11 +0100

    Changed modal modify to use Validators.

--- ClientApp/src/app/modal-modify/modal-modify.component.ts
+++ ClientApp/src/app/modal-modify/modal-modify.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, EventEmitter, Inject, Input, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Inject, Input, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiCaller } from '../api-caller.service';
 import {Boat} from '../boat'
 
@@ -9,22 +10,34 @@ import {Boat} from '../boat'
 })
 export class ModalModifyComponent {
 
+  private form: FormGroup;
   @Input() boat: Boat;
-  @Output() retboat: EventEmitter<Boat> = new EventEmitter<Boat>();
-  @ViewChild('name', { static: false }) nameInput: ElementRef;
-  @ViewChild('desc', { static: false }) descInput: ElementRef;
 
-  constructor(private apiCaller: ApiCaller) { }
+  // Used to detect when Boat gets a value to properly initialize the form.
+  ngOnChanges(changes: SimpleChanges) {
+    this.form = this.fb.group({
+      inputBoatName: [changes.boat.currentValue.name, Validators.required],
+      inputBoatDesc: [changes.boat.currentValue.desc, Validators.required]
+    });
+  }
+
+  @Output() retboat: EventEmitter<Boat> = new EventEmitter<Boat>();
+
+  constructor(private fb: FormBuilder, private apiCaller: ApiCaller) {}
 
   saveChanges(): void {
     var newBoat: Boat = Object.assign({}, this.boat);
-    newBoat.name = this.nameInput.nativeElement.value;
-    newBoat.desc = this.descInput.nativeElement.value;
-    this.apiCaller.updateBoat(newBoat).subscribe(r => {
-      this.retboat.emit(newBoat);
-      console.log("Success put");
-      this.dismiss();
-    }, err => console.error(err));
+    const val = this.form.value;
+
+    if (this.form.valid) {
+      newBoat.name = val.inputBoatName;
+      newBoat.desc = val.inputBoatDesc;
+      this.apiCaller.updateBoat(newBoat).subscribe(r => {
+        this.retboat.emit(newBoat);
+        console.log("Success put");
+        this.dismiss();
+      }, err => console.error(err));
+    }
   }
 
   dismiss(): void {
